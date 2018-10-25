d1e12945db63e36a568bc196b1856a01d5034e72
Bert Frees <bertfrees@gmail.com>
2018-10-25 12:54:09 +0200

    Rename files

---
+++ modules/sbs/dtbook-to-ebook/src/test/java/XProcSpecTest.java
@@ -0,0 +1,17 @@
+import org.daisy.pipeline.junit.AbstractXSpecAndXProcSpecTest;
+
+import static org.daisy.pipeline.pax.exam.Options.thisPlatform;
+
+public class XProcSpecTest extends AbstractXSpecAndXProcSpecTest {
+
+	@Override
+	protected String[] testDependencies() {
+		return new String[]{
+			"org.daisy.pipeline.modules:nordic-epub3-dtbook-migrator:?",
+			"org.daisy.pipeline.modules.braille:epub3-to-epub3:?",
+			"org.daisy.pipeline.modules:dtbook-utils:?",
+			"org.daisy.pipeline.modules:file-utils:?",
+			"org.daisy.pipeline.modules:fileset-utils:?",
+		};
+	}
+}
